7483ea8871e41be83a5ae51c66edbd339ce4f9dc
shubham <shubhamkumar10516@gmail.com>
2020-10-07 11:47:49 +0530

    [Shubham]REFACTORED: uc4 taking user input where the empty and specified positon on the board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,17 +1,64 @@
 package com.bridgeLab.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
-	
-	public static void assignEmptySpace(char[] ch) {
-		
-		for(int i = 1 ; i < ch.length ; i++)
+	private static Scanner sc = new Scanner(System.in);
+
+	// assigning empty space to board
+	public static void assignBoardEmptySpace(char[] ch) {
+
+		for (int i = 1; i < ch.length; i++)
 			ch[i] = ' ';
 	}
+
+	// taking input from user
+	public static char chooseInput() {
+
+		System.out.println("Enter the choice x or o");
+		char input = sc.next().charAt(0);
+		return input;
+	}
+
+	// printing board
+	public static void showBoard(char[] board) {
+
+		System.out.println("---Show board---");
+		for (int i = 1; i <= 3; i++)
+			System.out.print(i + ": |" + board[i] + "| ");
+		System.out.println();
+		for (int i = 4; i <= 6; i++)
+			System.out.print(i + ": |" + board[i] + "| ");
+		System.out.println();
+		for (int i = 7; i <= 9; i++)
+			System.out.print(i + ": |" + board[i] + "| ");
+
+	}
+
+	private static void inputAtEmptyPosition(char[] board, char userInput) {
+		System.out.println("Give specify position: (1 -9) ");
+		int pos = sc.nextInt();
+		while (true) {
+			if (board[pos] == ' ') {
+				board[pos] = userInput;
+				break;
+			}
+			if (board[pos] != ' ')
+				System.out.println("Position filled..choose different position");
+
+		}
+	}
+
 	public static void main(String[] args) {
-		
-	    char[] ch = new char[10];
-		assignEmptySpace(ch);
+
+		char[] board = new char[10];
+		assignBoardEmptySpace(board);
+		char userInput = chooseInput();
+		char computerInput = userInput == 'x' ? 'o' : 'x';
+		showBoard(board);
+		inputAtEmptyPosition(board, userInput);
+		showBoard(board);
 	}
 
 }
