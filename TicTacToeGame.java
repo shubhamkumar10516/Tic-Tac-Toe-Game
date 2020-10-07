6c46dca1aa5fe96c63efcb160f838c782421397b
shubham <shubhamkumar10516@gmail.com>
2020-10-07 10:36:47 +0530

    [Shubham]ADDED: show board method added

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,24 +4,34 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 
-	public static Scanner sc = new Scanner(System.in);
-	//assigning empty space to board
+	private static Scanner sc = new Scanner(System.in);
+
+	// assigning empty space to board
 	public static void assignBoardEmptySpace(char[] ch) {
-		
-		for(int i = 1 ; i < ch.length ; i++)
+        System.out.println("---Show board---");
+		for (int i = 1; i < ch.length; i++)
 			ch[i] = ' ';
 	}
-	//taking input from user
+
+	// taking input from user
 	public static char chooseInput() {
-		
+
 		System.out.println("Enter the choice x or o");
-		char input =  sc.next().charAt(0);
+		char input = sc.next().charAt(0);
 		return input;
 	}
+
+	// printing board
+	public static void showBoard(char[] ch) {
+		for(int i = 0 ; i < ch.length ; i++)
+			System.out.print(ch[i]+" ");
+	}
 	public static void main(String[] args) {
-		
-	    char[] board = new char[10];
+
+		char[] board = new char[10];
 		assignBoardEmptySpace(board);
-		char input = chooseInput();
+		char userInput = chooseInput();
+		char computerInput = userInput == 'x' ? 'o':'x';
+		showBoard(board);
 	}
 }
