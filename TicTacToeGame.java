ff7bd521e1604333ff9937927e2ed3f86d9dd58b
shubham <shubhamkumar10516@gmail.com>
2020-10-07 09:49:53 +0530

    [Shubham]ADDED: taken input from user as x or o

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,17 +1,24 @@
 package com.bridgeLab.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
-	
-	public static void assignEmptySpace(char[] ch) {
+	public static Scanner sc = new Scanner(System.in);
+	public static void assignBoardEmptySpace(char[] ch) {
 		
 		for(int i = 1 ; i < ch.length ; i++)
 			ch[i] = ' ';
 	}
+	public static char chooseInput() {
+		System.out.println("Enter the choice x or o");
+		char input =  sc.next().charAt(0);
+		return input;
+	}
 	public static void main(String[] args) {
 		
-	    char[] ch = new char[10];
-		assignEmptySpace(ch);
+	    char[] board = new char[10];
+		assignBoardEmptySpace(board);
+		char input = chooseInput();
 	}
-
 }
