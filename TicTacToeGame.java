9dc92f693d0ed0ed318eceab2887f3ca52b8e200
shubham <shubhamkumar10516@gmail.com>
2020-10-07 09:54:00 +0530

    [Shubham]ADDED: taken input from user as x or o

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class TicTacToeGame {
 
 	public static Scanner sc = new Scanner(System.in);
+	//assigning empty space to board
 	public static void assignBoardEmptySpace(char[] ch) {
 		
 		for(int i = 1 ; i < ch.length ; i++)
 			ch[i] = ' ';
 	}
+	//taking input from user
 	public static char chooseInput() {
+		
 		System.out.println("Enter the choice x or o");
 		char input =  sc.next().charAt(0);
 		return input;
